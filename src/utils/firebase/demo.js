68b1552af99245a7fa3a154179773761095047c2
paras <parasgoel111@gmail.com>
2022-12-15 23:26:00 +0530

    Signup and Authentication component add

---
+++ src/utils/firebase/demo.js
@@ -0,0 +1,27 @@
+let doc = {
+  names:   "paras", 
+  dresscode : "Abes",
+  timing :"9-4:30"
+}
+dep ="Cs";
+
+// const {...otherprops}=  doc;
+
+let names  = " "
+
+// console.log({names ,...doc}); 
+
+
+
+
+
+
+// function display (addinfo={})
+// {
+   
+//   console.log(addinfo)
+// console.log({dep,...addinfo});
+
+// }
+
+
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,8 +1,7 @@
 import {initializeApp} from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { doc ,  getFirestore, setDoc,getDoc, Firestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup ,createUserWithEmailAndPassword ,signInWithEmailAndPassword} from "firebase/auth";
+import { doc ,  getFirestore, setDoc,getDoc,   } from "firebase/firestore";
 
-//  export {createUserDocumentFromAuth} from  "./firebase.createDoc"
 
 
 const firebaseConfig =
@@ -15,45 +14,78 @@ const firebaseConfig =
     appId: "1:993252936708:web:985d70fdfbeb64301a3951" 
   };
 
-const app = initializeApp(firebaseConfig); // Initialisation step  firebaseconfig is the Projects id at firebase server
-// console.log(app ,"this is the application ") 
 
-const provider  = new GoogleAuthProvider(); // initialising the google provider on client side
-
-  provider.setCustomParameters(     // this will internally open the prompt setting for the select account
+const provider  = new GoogleAuthProvider(); 
+// initialising the google provider on client side
+  provider.setCustomParameters(   // this will internally open the prompt setting for the select account
     {
     prompt:'select_account' 
     }
-   )
+)
+//                                                                INITIALISE                                                                
 
-  export const auth = getAuth();    //  its basically a single tabel
-  
-  export const signInGooglePopup = ()=>{
+// console.log(app ,"this is the application ") 
+const app = initializeApp(firebaseConfig);  // Connnect to firebase 
+
+export const db =  getFirestore(); // Intialise DataBase 
+export const auth = getAuth(app);    //  intialise Autherization 
+
+ //                                                          Sign in with Google 
+
+  export const signInGooglePopup = ()=>
+  {
   return signInWithPopup(auth,provider); // auth me check kro if provider is present then give the information  and if not present make one
   }
 
-  export const db =  getFirestore();
-  export const createUserDocumentFromAuth = 
-  async (userAuth)=>
-  {
-      const userRef = doc(db, 'user', userAuth.uid);  // giving the document refrence inside db database in user collection with this uid
-      console.log(userRef)  // this is the document 
-      
-      const userSnapShot = await getDoc(userRef);
-      console.log(userSnapShot)
 
+ //                                                  Create    Creating Document                                                   
+
+  export const DocumentFromAuth = async (userAuth , additonalinfo={})=> //   {display : "name"} ==> additional ={display:"name"}
+{
+      const docRef = doc(db, 'user', userAuth.uid);  // Create Document  with user.uid as a name  of the document  
+      const userSnapShot = await getDoc(docRef); // it check document present or not 
+     
       if(!userSnapShot.exists())
       {
          const { email,displayName } = userAuth;
          const createdAt = new Date();
-
          try {
-          await setDoc(userRef ,{displayName ,email,createdAt}) ;
+            console.log(additonalinfo);
+            await setDoc(docRef ,{ displayName ,email,createdAt, ...additonalinfo});
 
          }
-         catch (error){
-            console.log("error is creating the user " , error)
+         catch (error){ 
+            console.log("error in creating the user " ,error)
          }
       }
-      return userRef
+      return docRef;
+  }
+
+  export const signInUserWithMailandPassword = async (email, password)=>
+  {
+
+   try{
+
+        
+     return  await signInWithEmailAndPassword(auth,email,password);
+   }
+   catch(error)
+   {
+      console.log(error.message)
+   }
+
+  }
+
+  export const createUserWithEmailAndPasswordAuth = async (email , password)=>
+  {
+     
+try{
+
+   return  await   createUserWithEmailAndPassword( auth ,email, password);
+}
+catch(error)
+{
+   console.log(error.message)
+}
+
   }
